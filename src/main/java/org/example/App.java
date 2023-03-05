d697ae3c79c8727d60266ae20d6def16afec7473
깃ChertHyeon <r4560798@naver.com>
2023-03-05 22:14:31 +0900

    8ë단계, 리포지토리 도입

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,35 +1,49 @@
 package org.example;
 
-import java.util.Scanner;
+import org.example.system.controller.SystemController;
+import org.example.wiseSaying.controller.WiseSayingController;
+import org.example.wiseSaying.entity.WiseSaying;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
 
 public class App {
-
-    private final Scanner sc;
-
-    public App(Scanner sc) {
-        this.sc = sc;
-        System.out.println("==명언 앱 ==");
-    }
-
     public void run() {
+        System.out.println("==명언 앱 ==");
+        long lastWiseSayingId = 1;
+        SystemController systemController = new SystemController();
+        WiseSayingController wiseSayingController = new WiseSayingController();
 
+        List<WiseSaying> WiseSayingList = new ArrayList<>();
         while (true) {
             System.out.print("명령) ");
             //trim() : 좌우 공백 없는 String 반환
-            String command = sc.nextLine().trim();
+            String command = Container.getScanner().nextLine().trim();
+            Rq rq = new Rq(command);
 
-            if (command.equals("종료")) {
-                break;
+            switch (rq.getActionCode()) {
+                case "종료":
+                    systemController.exit();
+                    return;
+
+                case "등록":
+                    wiseSayingController.write();
+                    break;
+
+                case "목록":
+                    wiseSayingController.list();
+                    break;
+
+                case "삭제":
+                    wiseSayingController.remove(rq);
+                    break;
+
+                case "수정":
+                    wiseSayingController.modify(rq);
+                    break;
             }
-
-            if (command.equals("등록"))
-            {
-                System.out.print("명언 :");
-                String content = sc.nextLine().trim();
-                System.out.print("작가 :");
-                String authorName = sc.next().trim();
-            }
-
         }
     }
 }
